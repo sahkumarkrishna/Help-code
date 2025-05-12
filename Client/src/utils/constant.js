31af8b0df47a8121bb8237a469fcbe24f2506f00
Aryan Jaiswal <aryan19jaiswal@gmail.com>
2025-05-12 22:11:08 +0530

    second commit

--- Client/src/utils/constant.js
+++ Client/src/utils/constant.js
@@ -1,2 +1,2 @@
-export const USER_API_END_POINT = "http://localhost:3000/api/v1/user";
-export const REVIEW_API_END_POINT = "http://localhost:3000/api/v1/core"
+export const USER_API_END_POINT = "https://brocode-server.onrender.com/api/v1/user";
+export const REVIEW_API_END_POINT = "https://brocode-server.onrender.com/api/v1/core"
